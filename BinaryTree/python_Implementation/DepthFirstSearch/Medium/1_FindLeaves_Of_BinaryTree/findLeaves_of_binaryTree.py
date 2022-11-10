db3877bb5b70de70b1bb1585fcea369f4b8754f6
ZhaoMingSoh <zhaomingsoh1999@gmail.com>
2022-11-10 15:50:11 +0800

    (M) completed leetcode (Medium-Py3) BinaryTree/1_FindLeaves_Of_BinaryTree

--- BinaryTree/python_Implementation/DepthFirstSearch/Medium/1_FindLeaves_Of_BinaryTree/findLeaves_of_binaryTree.py
+++ BinaryTree/python_Implementation/DepthFirstSearch/Medium/1_FindLeaves_Of_BinaryTree/findLeaves_of_binaryTree.py
@@ -3,34 +3,33 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-    
+
+
 def findLeaves(root: TreeNode) -> list[list[int]]:
     collectLeafNodes = []
-    while(root != None):
-        leafNodes = []
-        if root.left == None and root.right == None:
-            leafNodes.append(root.val)
-            collectLeafNodes.append(leafNodes)
-            break
+    # Bottom Up Approach :
+    def DFS_Recur(node):
+        # Null Node
+        if node == None:
+            return 0
 
-        def DFS_Recur(node, leafNodes):
-            print(node.val)
-            # Leaf node
-            if node.left == None and node.right == None:
-                leafNodes.append(node.val)
-                return
-                
-            # Delete the leaf node by setting its parent's either left or right branch to point to None
-            DFS_Recur(node.left,leafNodes)
-            node.left = None
-            DFS_Recur(node.right,leafNodes)
-            node.right = None
+        # PreOrder traversal : L -> R -> N
+        leftHeight = DFS_Recur(node.left)
+        rightHeight = DFS_Recur(node.right)
 
-            return
-        DFS_Recur(root, leafNodes)
-        print(leafNodes)
-        collectLeafNodes.append(leafNodes)
+        # To determine the height of the current node, find the max(left child, right child) height
+        # We're gonna use the height to determine if the array is of the right size
+        if len(collectLeafNodes) < max(leftHeight,rightHeight)+1:
+            # If not, we're gonna extend it, so that we can store the val at the position via height
+            collectLeafNodes.extend([[]])
+        
+        # If yes, simply store it in the right position
+        collectLeafNodes[max(leftHeight,rightHeight)].append(node.val)
 
+        # Increment the current node height's by 1 and return
+        return 1 + max(leftHeight,rightHeight)
+
+    DFS_Recur(root)
     return collectLeafNodes
 
 if __name__ == "__main__":
